e304338b729fcc4fd9f1bc08bf7118152ecc95d5
Swanand Kulkarni <kulswanand07@gmail.com>
2024-01-02 22:44:02 +0530

    Remove all files from branch

--- src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Profile</h1>
-      <hr />
-      <p>ProfilePage</p>
-    </div>
-  );
-}
